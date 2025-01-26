ae2f91e2f61bd419724f46aff2fd89531a98ea55
unknown <beridzegigi19@gmail.com>
2025-01-26 09:49:33 -0800

    Header

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 export default function LandingPage() {
   return (
-    <div className="hello">
-        <h1>Hello Landing Page</h1>
-        <button>Sign In</button>
+ 
+
+    <div className="landingpage flex bg-black w-[100%] flex items-center justify-center p-[10px]">
+      <div className="landingpageheader flex items-center justify-between w-[80%]">
+        <div className="left"><img width={120} src="Super5 logo.png" alt="" /></div>
+        <div className="right flex items-center justify-center gap-[20px] text-white">
+            <div className="landingregister">Not a Member Yet? <a href="/login">Register Now</a></div>
+            <a href='/login' className="landinglogin p-[5px] p-[5px]">Log In</a>
+        </div>
+      </div>
     </div>
+
+
+
   )
 }
