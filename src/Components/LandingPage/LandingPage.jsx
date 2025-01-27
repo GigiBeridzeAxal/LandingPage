ba6f4cccee4476cd5c26f6c855da05b686baeb02
unknown <beridzegigi19@gmail.com>
2025-01-27 00:25:23 -0800

    Responsive

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -41,7 +41,7 @@ export default function LandingPage() {
       <div className="landingpageheader flex items-center justify-between w-[80%]">
         <div className="left"><img width={120} src="Super5 logo.png" alt="" /></div>
         <div className="right flex items-center justify-center gap-[20px] text-white">
-            <div className="landingregister">Not a Member Yet? <a className='text-red-400' href="/login ">Register Now</a></div>
+            <div className="landingregister flex items-center justify-center gap-[10px]"> <div className="notmember">Not a Member Yet? </div> <a className='text-red-400' href="/login ">Register Now</a></div>
             <a href='/login' className="landinglogin p-[5px] p-[5px]">Log In</a>
         </div>
       </div>
