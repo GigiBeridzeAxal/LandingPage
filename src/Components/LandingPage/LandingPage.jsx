52b50d505e7ba6fe7143f4dc95a1a30373ea0cc9
unknown <beridzegigi19@gmail.com>
2025-01-28 05:56:17 -0800

    Responsive Footer

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -435,28 +435,28 @@ export default function LandingPage() {
       </div>
       <div className="footer w-[100%]  ">
         <div className="footerframe flex   p-[20px] justify-around w-[100%]">
-          <div className="firstline flex  flex-col"><img width={120} src="Super5 logo.png" alt="" />
-          <br /><br />
+          <div className="firstline flex  flex-col"><img width={160} src="Super5 logo.png" alt="" />
+          <br />
           <div className="socialstittle">Socials</div>
           <div className="socials flex items-center gap-[5px]"><img width={30} src="Facebook.png" alt="" /> <img width={30} src="Instagram.png" alt="" /></div>
           </div>
           <div className="navigate flex gap-[5px] flex-col">
             <strong className="navigatetittle text-[24px]">Navigate</strong>
-            <a href="">About Us</a>
-            <a href="">How to Play</a>
-            <a href="">FAQ</a>
-            <a href="">Contact us</a>
+            <a href="/www.trickit.now">About Us</a>
+            <a href="/www.trickit.now">How to Play</a>
+            <a href="/www.trickit.now">FAQ</a>
+            <a href="/www.trickit.now">Contact us</a>
           </div>
 
           <div className="navigate gap-[5px] flex flex-col">
             <strong className="navigatetittle text-[24px]">Support</strong>
-            <a href="">Terms & Conditions</a>
-            <a href="">Privacy policy</a>
+            <a href="/www.trickit.now">Terms & Conditions</a>
+            <a href="/www.trickit.now">Privacy policy</a>
           </div>
 
           <div className="navigate flex gap-[5px] flex-col">
             <strong className="navigatetittle text-[24px]">Contact</strong>
-            <a href="">Contact us</a>
+            <a href="/www.trickit.now">Contact us</a>
           </div>
  
         </div>
