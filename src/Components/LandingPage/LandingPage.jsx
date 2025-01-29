d972b4d2a943781aae9b64841328b24f1f9c7fcf
Gigi Beridze <145029127+GigiBeridzeAxal@users.noreply.github.com>
2025-01-29 10:26:24 +0400

    Update LandingPage.jsx

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -390,7 +390,7 @@ className="questions flex items-center ">
 
 
       </div>
-      <div className="footer w-[100%] bg-black text-white">
+      <div className="footer w-[100%] bg-gray-800 text-white">
         <div className="footerframe flex   p-[20px] justify-around w-[100%]">
           <div className="firstline flex  flex-col"><img width={135} src="Super5 logo.png" alt="" />
           <br />
@@ -420,7 +420,7 @@ className="questions flex items-center ">
           </div>
  
         </div>
-        <div className="copyright flex items-center justify-center">Copyright @2025 all rights reserved</div>
+        <div className="copyright bg-black flex items-center justify-center">Copyright @2025 all rights reserved</div>
       </div>
     </div>
    
